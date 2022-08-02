29eca2b3b86c737929d1194e255fb812bcf09801
Seongju-Lee <lsj40413@gmail.com>
2022-08-02 18:01:08 +0900

    login-confirm 수정

--- backend/webapps/auth/route_login.py
+++ backend/webapps/auth/route_login.py
@@ -65,7 +65,8 @@ def token_auth(input_auth: str='', user_id: str='', db: Session = Depends(get_db
 @router.get("/login")
 def login(request: Request, msg: str = None):
     print('param msg확인: ', msg)
-    msg='로그인 세션이 만료되었습니다.'
+    if msg:
+        msg='로그인 세션이 만료되었습니다.'
     return templates.TemplateResponse(
         "login.html", {"request": request, "msg": msg}
     )

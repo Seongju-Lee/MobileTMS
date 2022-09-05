8400d80b5d73181f8ce053d16fc6ba57a4ea3c72
Seongju-Lee <lsj40413@gmail.com>
2022-09-05 18:32:44 +0900

    모델검색 필터 UI

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from apis.version1 import route_login
+from apis.version1 import route_models
 
 api_router = APIRouter()
 
-api_router.include_router(route_login.router, prefix="/user", tags=["login"])
+api_router.include_router(route_login.router, prefix="/user", tags=["로그인"])
+api_router.include_router(route_models.router, prefix="/models", tags=["모델검색 관련"])
